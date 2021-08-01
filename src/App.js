0e08f7cf16ebd790f494887a30dc77781f666f5c
Matthew Lean <matt@mattlean.com>
2021-08-01 03:00:17 -0700

    Generate multiple faces each with random features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import Face from './Face'
 import './App.css'
+import { range } from 'd3'
 
-const width = 960
-const height = 500
+const width = 150
+const height = 150
+
+const array = range(36)
 
 function App() {
-  return (
+  return array.map(() => (
     <Face
       centerX={width / 2}
       centerY={height / 2}
-      eyeOffsetX={90}
-      eyeOffsetY={110}
-      eyeRadius={40}
+      eyeOffsetX={20 + Math.random() * 9}
+      eyeOffsetY={20 + Math.random() * 15}
+      eyeRadius={5 + Math.random() * 10}
       height={height}
-      mouthRadius={140}
-      mouthWidth={20}
-      strokeWidth={20}
+      mouthRadius={30 + Math.random() * 10}
+      mouthWidth={7 + Math.random() * 9}
+      strokeWidth={6 + Math.random() * 3}
       width={width}
     />
-  )
+  ))
 }
 
 export default App
